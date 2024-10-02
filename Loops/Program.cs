54a77a6e36ee385f06bdf4fa4925ce3d4d46d7a0
Liam Rutherford <liamrutherford10@gmail.com>
2024-10-02 17:33:36 +0100

    Add project files.

---
+++ Loops/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; // Loops
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("{0}", i);
+        }
+        Console.WriteLine();
+        for (int j = 10; j > 0; j--)
+        {
+            Console.WriteLine("{0}", j);
+        }
+    }
+}
